bf37aad945670828db80761ba050fdd6061bce77
BLR <bruff@Macky-Max.local>
2024-12-02 18:47:36 -0500

    this feature allows Check if the 2D matrix is a magic square.

---
+++ junittest.java
@@ -0,0 +1,75 @@
+
+class junittest {
+
+    void testIsMagicSquareValid() {
+        int[][] matrix = {
+            {8, 1, 6},
+            {3, 5, 7},
+            {4, 9, 2}
+        };
+        assertTrue(main.isMagicSquare(matrix), "Matrix should be identified as a magic square");
+            }
+        
+            private void assertTrue(boolean magicSquare, String string) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'assertTrue'");
+            }
+        
+            
+    void testIsMagicSquareInvalidRow() {
+        int[][] matrix = {
+            {8, 1, 6},
+            {3, 5, 7},
+            {4, 9, 3}
+        };
+        assertFalse(main.isMagicSquare(matrix), "Matrix should not be identified as a magic square due to row sum mismatch");
+            }
+        
+        
+            private void assertFalse(boolean magicSquare, String string) {
+                // TODO Auto-generated method stub
+                throw new UnsupportedOperationException("Unimplemented method 'assertFalse'");
+            }
+        
+            void testIsMagicSquareInvalidColumn() {
+        int[][] matrix = {
+            {8, 1, 6},
+            {3, 5, 7},
+            {5, 9, 2}
+        };
+        assertFalse(main.isMagicSquare(matrix), "Matrix should not be identified as a magic square due to column sum mismatch");
+    }
+
+    
+    void testIsMagicSquareInvalidDiagonal() {
+        int[][] matrix = {
+            {8, 1, 6},
+            {3, 5, 7},
+            {4, 9, 3}
+        };
+        assertFalse(main.isMagicSquare(matrix), "Matrix should not be identified as a magic square due to diagonal sum mismatch");
+    }
+
+    
+    void testIsMagicSquareSingleElement() {
+        int[][] matrix = {
+            {5}
+        };
+        assertTrue(main.isMagicSquare(matrix), "Single-element matrix should be identified as a magic square");
+    }
+
+    
+    void testIsMagicSquareEmptyMatrix() {
+        int[][] matrix = {};
+        assertFalse(main.isMagicSquare(matrix), "Empty matrix should not be identified as a magic square");
+    }
+
+    
+    void testIsMagicSquareNonSquareMatrix() {
+        int[][] matrix = {
+            {8, 1, 6},
+            {3, 5, 7}
+        };
+
+    }
+}
--- main.java
+++ main.java
@@ -4,5 +4,60 @@
  * 2. A method to count the number of unique subarrays that sum to a given value.
  * 3. A method to find the longest palindromic substring in a given string.
  */
-public class UtilityFunctions {
-}
+
+ public class main {
+
+    /**
+     * Check if the 2D matrix is a magic square.
+     *
+     * @param matrix the 2D array 
+     * @return if the magic square
+     */
+    public static boolean isMagicSquare(int[][] matrix) {
+        int n = matrix.length;
+        int magicSum = 0;
+
+        // Calculating the magic sum 
+        for (int j = 0; j < n; j++) {
+            magicSum += matrix[0][j];
+        }
+
+        // Checking sum of rows
+        for (int i = 1; i < n; i++) {
+            int rowSum = 0;
+            for (int j = 0; j < n; j++) {
+                rowSum += matrix[i][j];
+            }
+            if (rowSum != magicSum) {
+                return false;
+            }
+        }
+
+        // Checking sum of columns
+        for (int j = 0; j < n; j++) {
+            int colSum = 0;
+            for (int i = 0; i < n; i++) {
+                colSum += matrix[i][j];
+            }
+            if (colSum != magicSum) {
+                return false;
+            }
+        }
+
+        // Checking main 
+        int diagonal1Sum = 0;
+        for (int i = 0; i < n; i++) {
+            diagonal1Sum += matrix[i][i];
+        }
+        if (diagonal1Sum != magicSum) {
+            return false;
+        }
+
+        // Checking secondary 
+        int diagonal2Sum = 0;
+        for (int i = 0; i < n; i++) {
+            diagonal2Sum += matrix[i][n - 1 - i];
+        }
+        return diagonal2Sum == magicSum;
+    }
+ }
