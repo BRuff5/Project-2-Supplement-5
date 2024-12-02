8a734eebacfe78db37165635d9f94e06e77d87ef
BLR <bruff@Macky-Max.local>
2024-12-02 17:32:40 -0500

    first commit

---
+++ main.java
@@ -0,0 +1,8 @@
+/**
+ * This class contains three functionalities:
+ * 1. A static method determining if a 2D matrix is a magic square.
+ * 2. A method to count the number of unique subarrays that sum to a given value.
+ * 3. A method to find the longest palindromic substring in a given string.
+ */
+public class UtilityFunctions {
+}
